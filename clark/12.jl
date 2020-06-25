6c3e845c225a091ce2edc54990fd84b367407a19
Clark Fitzgerald <clarkfitzg@gmail.com>
2020-06-25 10:01:18 -0700

    define functions for 12

---
+++ clark/12.jl
@@ -0,0 +1,26 @@
+import Primes: factor
+
+
+"""
+Count the number of factors of x.
+For example, 28 has 6 factors: 1,2,4,7,14,28.
+
+```julia-repl
+julia> count_factors(28)
+6
+```
+"""
+function count_factors(x)
+end
+
+
+"""
+Find the first triangle number with more than n divisors
+
+```julia-repl
+julia> first_div_triangle_num(5)
+28
+```
+"""
+function first_div_triangle_num(n = 500)
+end
