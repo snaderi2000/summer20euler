141578da8946c27341f5febab12b52a5b59be6da
Clark Fitzgerald <clarkfitzg@gmail.com>
2020-07-03 09:03:29 -0700

    concise, hopefully efficient divisors function

--- clark/21.jl
+++ clark/21.jl
@@ -1,6 +1,30 @@
 using Primes
 
 """
+Find all divisors of n
+
+```julia-repl
+julia> sort(collect(divisors(12)))
+6-element Array{Int64,1}:
+  1
+  2
+  3
+  4
+  6
+ 12
+```
+"""
+divisors = function(n)
+    f = factor(n)
+    eachpower = (0:k for k in values(f))
+    powers = Iterators.product(eachpower...)
+    div = (prod(keys(f) .^ p) for p in powers)
+    #Iterators.flatten(div)
+    div
+end
+
+
+"""
 Sum of the proper divisors of n
 
 ```julia-repl
@@ -9,11 +33,5 @@ julia> sum_divisors(220)
 ```
 """
 sum_divisors = function(n)
-    f = factor(n)
-    fl = []
-    for (fac, mul) in f
-        for i in 1:mul
-            push!(fl, fac)
-        end
-    end
+    sum(divisors(n)) - n
 end
